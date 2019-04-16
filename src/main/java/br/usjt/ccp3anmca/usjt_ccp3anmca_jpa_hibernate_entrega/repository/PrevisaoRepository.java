0205debbea3fb1b4a9d17dadb166f86f53eb1cd9
LucasGustavo088 <lucas.gustavo088@gmail.com>
2019-04-16 20:53:20 -0300

    Update PrevisaoRepository.java

--- src/main/java/br/usjt/ccp3anmca/usjt_ccp3anmca_jpa_hibernate_entrega/repository/PrevisaoRepository.java
+++ src/main/java/br/usjt/ccp3anmca/usjt_ccp3anmca_jpa_hibernate_entrega/repository/PrevisaoRepository.java
@@ -5,7 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.usjt.ccp3anmca.usjt_ccp3anmca_jpa_hibernate_entrega.model.Previsao;
 
 public interface PrevisaoRepository extends JpaRepository<Previsao, Long>{
-  public Cidade findByLatitudeAndLongitude(double latitude, double longitude);
-	public Cidade findByNome(String nome);
-	public List <Cidade> findByNomeInIgnoreCase(String nome);
+ 
 }
